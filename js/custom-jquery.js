2c9e5e288f95f15531f806572313f70ace77151d
alpha <arashtadcompany@gmail.com>
2022-11-21 15:35:36 +0400

    uploading...

---
+++ js/custom-jquery.js
@@ -0,0 +1,70 @@
+/***
+  __    ___    __    __   _    _____   __    ___
+ / /\  | |_)  / /\  ( (` | |_|  | |   / /\  | | \
+/_/--\ |_| \ /_/--\ _)_) |_| |  |_|  /_/--\ |_|_/
+
+*** Product Name:    Sci-Fi Game UI
+*** Description:     UI elements pack for Sci-Fi games.
+*** Author:          Arashtad
+*** Author URI:      https://arashtad.com
+*** Version:         1.0.0
+*** License:         GNU General Public License V3
+*** Copyright:       (C) 2022 - Arashtad.com. All Rights Reserved.
+
+***/
+
+$(document).ready(function() {
+    // Changing the default browser scrollbar ¯\_(ツ)_/¯
+    $(function() {
+        $('html, body, .ar-slider-list > li').overlayScrollbars({ });
+    });
+
+    $('.ar-slider').each(function() {
+        var $slider = $(this);
+        var len = $slider.find('li').length;
+        var prev = $slider.find('.ar-prev');
+        var next = $slider.find('.ar-next');
+        $slider.find('li:first-child').addClass('active');
+        var active = 1;
+        $(prev).click(function() {
+            if(active === 1) {
+                $slider.find('li:first-child').removeClass('active');
+                $slider.find('li:last-child').addClass('active');
+                active = len;
+            }
+            else {
+                $slider.find('li:nth-child(' + active + ')').removeClass('active');
+                active--;
+                $slider.find('li:nth-child(' + active + ')').addClass('active');
+            }
+        });
+        $(next).click(function() {
+            if(active === len) {
+                $slider.find('li:last-child').removeClass('active');
+                $slider.find('li:first-child').addClass('active');
+                active = 1;
+            }
+            else {
+                $slider.find('li:nth-child(' + active + ')').removeClass('active');
+                active++;
+                $slider.find('li:nth-child(' + active + ')').addClass('active');
+            }
+        });
+    });
+
+    $('.ar-tabs').each(function() {
+        var $tabs = $(this);
+        var tabLen = $tabs.find('.ar-tabs-buttons').find('li').length;
+        var activeTab = 1;
+        $tabs.find('.ar-tabs-buttons').find('li:first-child').addClass('active');
+        $tabs.find('.ar-tabs-content').find('li:first-child').addClass('active');
+
+        $tabs.find('.ar-tabs-buttons > li').click(function() {
+            activeTab = $(this).index() + 1;
+            $tabs.find('.ar-tabs-buttons').find('li').removeClass('active');
+            $tabs.find('.ar-tabs-content').find('li').removeClass('active');
+            $tabs.find('.ar-tabs-buttons').find('li:nth-child(' + activeTab + ')').addClass('active');
+            $tabs.find('.ar-tabs-content').find('li:nth-child(' + activeTab + ')').addClass('active');
+        });
+    });
+});
